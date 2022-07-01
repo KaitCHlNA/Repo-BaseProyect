6ede67f4952cc7fbbd954d738a86a0093f37505c
KaitCHlNA <kaits.orrego@gmail.com>
2022-07-01 18:23:05 -0400

    v0.0.5

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,37 +1,79 @@
-using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 5f;
-    public float horMove;
-    public float verMove;
-    //public AudioSource footSteps;
+    
+    //cámaras
+    public GameObject camRE;
+    public GameObject camThirdP;
+    //public GameObject camFirstP;
+
+    void Start()
+    {
+        Debug.Log("Hi there, to change the camera view press P");
+    }
     
     void Update()
     {
         Movement();
-        //InstantiateSound();
+        ChangeCams();
+        //CamsIf();
     }
 
     void Movement()
     {
-        horMove = Input.GetAxis("Horizontal");
-        verMove = Input.GetAxis("Vertical");
+        float horMove = Input.GetAxis("Horizontal");
+        float verMove = Input.GetAxis("Vertical");
 
         transform.Translate(new Vector3(horMove,0,verMove) * playerSpeed * Time.deltaTime);
     }
-/*
-    void InstantiateSound()
+
+    void ToggleCams()
     {
-        if (horMove != 0 || verMove != 0)
+        if (camRE.activeInHierarchy)
         {
-            Instantiate(footSteps);
+            camRE.SetActive(false);
+            camThirdP.SetActive(true);
         }
         else
         {
-            Destroy(footSteps);
+            camRE.SetActive(true);
+            camThirdP.SetActive(false);
         }
     }
+
+    void ChangeCams()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ToggleCams();
+        }
+    }
+    
+    /*
+    void CamsIf()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            camRE.SetActive(true);
+            camThirdP.SetActive(false);
+            camFirstP.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            camRE.SetActive(false);
+            camThirdP.SetActive(true);
+            camFirstP.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            camRE.SetActive(false);
+            camThirdP.SetActive(false);
+            camFirstP.SetActive(true);
+        }
+    } --> Nota, esto no es cómodo, implementar switch.
     */
 }
